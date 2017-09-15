03ea4fd23b43a1ad9b42cde8d3493e306b94f8da
Method <method@Methods-MacBook-Air.local>
2017-09-14 21:31:05 -0700

    fun times

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Link from 'next/link'
 const Index = () => (
   <div>
     <Link href="/about">
+      <a style={{ fontSize: 20 }}>About Page</a>
+    </Link><br/>
+    <Link href="/about" style={{ fontSize: 20 }}>
       <a>About Page</a>
     </Link>
     <p>Hello Next.js</p>
